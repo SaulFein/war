fb65da6ecf033020b25fae31a4bb187f64239337
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2016-01-31 22:07:30 -0800

    added in eslint and game now takes player name.

--- app.js
+++ app.js
@@ -1,67 +1,87 @@
-var cardsInSuit = ['two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king', 'ace'];
+(function(module) {
 
-var deck = [];
-var deck1 = [];
-var deck2 = [];
+  var cardsInSuit = ['two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king', 'ace'];
 
-function Card(suit, value, name, img) {
-  this.suit = suit;
-  this.value = value;
-  this.name = name;
-  this.img = img;
-}
-
-function createSuit(suit){
-  for (var i = 0; i < 13; i++) {
-    // console.log(cardsInSuit[i] + suit);
-    deck.push(new Card ( suit, parseInt([i]) + 2, cardsInSuit[i] + " of " + suit, '<img src="img/' + cardsInSuit[i] + suit + '.png">'));
-  }
-}
-
-function shuffleDeck () {
-  deck.sort(function() { return 0.5 - Math.random() });
-  for (var i = 0; i < deck.length; i += 2){
-      deck1.push(deck[i]);
-      deck[i + 1] && deck2.push(deck[i + 1]);
-  };
-}
-
+  var deck = [];
+  var deck1 = [];
+  var deck2 = [];
   var i = 0;
   var j = 0;
+  var p1s = 0;
+  var p2s = 0;
+  var playerOne;
 
-  function displayWinner() {
+  function Card(suit, value, name, img) {
+    this.suit = suit;
+    this.value = value;
+    this.name = name;
+    this.img = img;
+  }
+
+  Card.takeUserInput = function(){
+    // $('#userinput').show().siblings().hide();
+    playerOne = $('#userinputfield').val();
+    console.log(playerOne);
+  };
+
+  Card.createSuit = function(suit){
+    for (var i = 0; i < 13; i++) {
+      deck.push(new Card ( suit, parseInt([i]) + 2, cardsInSuit[i] + ' of ' + suit, '<img src="img/' + cardsInSuit[i] + suit + '.png">'));
+    }
+  };
+
+  Card.shuffleDeck = function() {
+    deck.sort(function() { return 0.5 - Math.random();});
+    for (var i = 0; i < deck.length; i += 2){
+      deck1.push(deck[i]);
+      deck[i + 1] && deck2.push(deck[i + 1]);
+    };
+  };
+
+  Card.displayWinner = function() {
+    $('#warspace').append(' ' + deck2[j].img);
     if(deck1[i].value > deck2[j].value) {
-      $('#winner').text("player one wins the round");
+      $('#winner').text(playerOne + ' wins the round');
+      p1s++;
     } else {
-      $('#winner').text("player two wins the round");
+      $('#winner').text('computer wins the round');
+      p2s++;
     }
     console.log(deck1[i].value, deck2[j].value);
-  }
+  };
 
+  Card.gameOver = function() {
+    alert('game over! ' + playerOne + ' score = ' + p1s + ' Computer score ' + p2s);
+    $('#next').hide();
+    var $ngb = $('<input type="button" value="New Game" />');
+    $ngb.appendTo($('#newgame'));
+    $('#newgame').show();
+    $ngb.on('click', function(){
+      window.location.reload();
+    });
+  };
 
-$('#next').on('click', function() {
-    i += 1;
-    j += 1;
-    $('#winner').text("");
-    if(i == 26){
-      alert("game over!");
-      $('#next').hide();
-      var $ngb = $('<input type="button" value="New Game" />');
-      $ngb.appendTo($("#newgame"));
-      $('#newgame').show();
-      $ngb.on('click', function(){
-        window.location.reload();
-      });
-    } else {
-      $('#warspace').html(deck1[i].img + " " + "vs" + " " + deck2[j].img);
-    // $('#warspace').html(this.img);
-      setTimeout('displayWinner()', 500)
+  Card.playGame = function() {
+    $('#next').on('click', function() {
+      i += 1;
+      j += 1;
+      $('#winner').text('');
+      if(i == 26){
+        Card.gameOver();
+      } else {
+        $('#warspace').html(deck1[i].img + ' ' + 'vs');
+        setTimeout('Card.displayWinner()', 500);
+      }
     }
-  }
-);
+  );};
 
-createSuit("Clubs");
-createSuit("Spades");
-createSuit("Diamonds");
-createSuit("Hearts");
-shuffleDeck();
+  // Card.takeUserInput();
+  Card.playGame();
+  Card.createSuit('Clubs');
+  Card.createSuit('Spades');
+  Card.createSuit('Diamonds');
+  Card.createSuit('Hearts');
+  Card.shuffleDeck();
+
+  module.Card = Card;
+})(window);
